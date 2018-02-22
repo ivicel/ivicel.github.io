47c96a6942930ce87787c2a913b5910542c75080
ivicel <31336040+ivicel@users.noreply.github.com>
2018-02-22 23:36:53 +0800

    generate blog at Thu Feb 22 23:36:53 CST 2018

--- do_all_thing.sh
+++ do_all_thing.sh
@@ -3,7 +3,7 @@
 pelican -s pelicanconf.py -o ivicel.github.io -d -D
 git add .
 git commit -m "generate blog at $(date)"
-git push all
+git push 
 #git --work-tree ivicel.github.io --git-dir .git/modules/ivicel.github.io add .
 #git --work-tree ivicel.github.io --git-dir .git/modules/ivicel.github.io commit -m 'generate blog'
 #git --work-tree ivicel.github.io --git-dir .git/modules/ivicel.github.io push
