ece4cb398b0e4c1cfc3aaeac87a63ae346744af5
ivicel <31336040+ivicel@users.noreply.github.com>
2019-05-05 01:31:28 +0800

    generate blog at Sun May  5 01:31:28 CST 2019

--- do_all_thing.sh
+++ do_all_thing.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-pelican -s pelicanconf.py -o output -t themes/attila -D
-git --work-tree output --git-dir ivicel.github.io.git add .
-git --work-tree output --git-dir ivicel.github.io.git commit -m "generate blog $(date)"
-git --work-tree output --git-dir ivicel.github.io.git push origin master
+pelican -s pelicanconf.py -o ivicel.info -t themes/attila -D
+git --work-tree ivicel.info --git-dir ivicel.github.io.git add .
+git --work-tree ivicel.info --git-dir ivicel.github.io.git commit -m "generate blog $(date)"
+git --work-tree ivicel.info --git-dir ivicel.github.io.git push origin master
 git add .
 git commit -m "generate blog at $(date)"
 git push origin master
